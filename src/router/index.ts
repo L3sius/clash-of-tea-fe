de25b80450102f105f5b56a64166f2edf524106e
Justas Leščinskas <justaslescinskas@gmail.com>
2026-01-22 13:28:45 +0200

    Add FAQ page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Faq from '@/components/Faq.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
+  { path: '/faq', name: 'FAQ', component: Faq}
 ];
 
 const router = createRouter({
